910b394c4d649c6c91daeff7b81e6553ce3b16be
Aleksey Guryanov <alexey@gurianov.com>
2015-07-13 19:00:19 +0000

    feat: bemModDirective and bemEventDirective initial implementations

--- angular-bem.js
+++ angular-bem.js
@@ -11,7 +11,9 @@ modules.define('angular-bem',
             .value('bemdom', BEMDOM)
             .factory('ngbem', ngBemFactory)
             .directive('ngBem', ngBemDirective)
-            .directive('bem', iBemDirective);
+            .directive('bem', iBemDirective)
+            .directive('bemMod', bemModDirective)
+            .directive('bemEvent', bemEventDirective);
 
         function ngBemFactory($compile) {
             var service = { render : render };
@@ -39,7 +41,7 @@ modules.define('angular-bem',
         function bemController($scope, $attrs, $element) {
             this.bem = $scope.$eval($attrs.bem);
 
-            angular.forEach(this.bem, function(v, k){
+            angular.forEach(this.bem, function(v, k) {
                 this.bem[k] = $($element).bem(k);
             }, this);
         }
@@ -55,7 +57,7 @@ modules.define('angular-bem',
 
                     if(!ngModel) return;
 
-                    angular.forEach(iBem.bem, function(v, k){
+                    angular.forEach(iBem.bem, function(v, k) {
                         iBem.bem[k].on('change', function(e, data) {
                             if(data && data.source === 'ng-model') {
                                 scope.$evalAsync(setModelValue.bind(iBem.bem[k]));
@@ -69,7 +71,7 @@ modules.define('angular-bem',
 
                     ngModel.$render = function() {
                         var value = angular.isUndefined(ngModel.$viewValue)? '' : ngModel.$viewValue;
-                        angular.forEach(iBem.bem, function(v, k){
+                        angular.forEach(iBem.bem, function(v, k) {
                             if(iBem.bem[k].setVal) iBem.bem[k].setVal(value, { source : 'ng-model' });
                         });
                     };
@@ -84,6 +86,88 @@ modules.define('angular-bem',
                     }
                 }
             };
+        }
+
+        function bemModDirective() {
+            return {
+                restrict : 'A',
+                require : 'bem',
+                link : function(scope, element, attrs, iBem) {
+                    var oldVal;
+
+                    scope.$watch(attrs.bemMod, bemModWatchAction, true);
+
+                    function setMods(newVal) {
+                        angular.forEach(newVal, function(v, block) {
+                            angular.forEach(v, function(modVal, modName) {
+                                this.setMod(modName, modVal);
+                            }, iBem.bem[block]);
+                        });
+                    }
+
+                    function bemModWatchAction(newVal) {
+                        if(!angular.equals(newVal, oldVal)) {
+                            setMods(newVal);
+                        }
+
+                        oldVal = shallowCopy(newVal);
+                    }
+                }
+            };
+        }
+
+        function bemEventDirective($parse) {
+            return {
+                restrict : 'A',
+                require : 'bem',
+                link : function(scope, element, attrs, iBem) {
+                    var events = scope.$eval(attrs.bemEvent);
+
+                    angular.forEach(events, function(eventsArray, block) {
+                        angular.forEach(eventsArray, function(eventDefenition) {
+                            var event,
+                                fn;
+
+                            if(eventDefenition.length === 2) {
+                                event = eventDefenition[0];
+                                fn = $parse(eventDefenition[1], null, true);
+                            } else if(eventDefenition.length === 3) {
+                                event = {
+                                    modName : eventDefenition[0],
+                                    modVal : eventDefenition[1]
+                                };
+                                fn = $parse(eventDefenition[2], null, true);
+                            }
+
+                            this.on(event, function() {
+                                scope.$apply(function() {
+                                    fn(scope);
+                                });
+                            });
+                        }, iBem.bem[block]);
+                    });
+                }
+            };
+        }
+
+        function shallowCopy(src, dst) {
+            if(angular.isArray(src)) {
+                dst = dst || [];
+
+                for(var i = 0, ii = src.length; i < ii; i++) {
+                    dst[i] = src[i];
+                }
+            } else if(angular.isObject(src)) {
+                dst = dst || {};
+
+                for(var key in src) {
+                    if(!(key.charAt(0) === '$' && key.charAt(1) === '$')) {
+                        dst[key] = src[key];
+                    }
+                }
+            }
+
+            return dst || src;
         }
 
         provide(angular.module('angular-bem'));
